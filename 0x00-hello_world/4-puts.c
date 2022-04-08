84a0b317ef538be8ec20ba7f1514ea48bf429918
abbot_jude@outlook.com <abbot_jude@outlook.com>
2022-04-08 02:49:26 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*this program, prints the inscription
+ *"PROGRAMMING IS LIKE BUILDING A MULTILINGUAL PUZZLE"
+ *on standard output.
+ */
+int main(void)
+
+{
+  puts("Programming is like building a multilingual puzzle.");
+
+    return (0);
+}
