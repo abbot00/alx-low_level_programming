b894315a0516dd133cc245c0f6439659f40c557f
abbot00 <abbot_jude@outlook.com>
2022-04-08 12:35:17 +0000

    corrections

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  *main - uses printf to print a set of strings and ends with a new line
- *Return: returns 0 when the programme runs correctly. 
+ *Return: returns 0 when the programme runs correctly
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
