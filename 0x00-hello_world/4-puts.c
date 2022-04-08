47b5a002348694f35e984bfa8ea129d3876d9656
abbot_jude@outlook.com <abbot_jude@outlook.com>
2022-04-08 03:38:14 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-/*this program, prints the inscription
- *"PROGRAMMING IS LIKE BUILDING A MULTILINGUAL PUZZLE"
- *on standard output.
+/** 
+ *main - this program, prints the inscription "PROGRAMMING IS LIKE BUILDING A MULTILINGUAL PUZZLE" on standard output.
+ *Return: 0 if exited properly, non-zero otherwise
  */
-/*this it the main part of the program.*/
+
 int main(void)
 
 {
-  puts("Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
 
     return (0);
 }
