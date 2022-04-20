55b20cd1540a21453013d1f3b4349f1bf70cf68f
abbot00 <abbot_jude@outlook.com>
2022-04-20 14:35:13 +0000

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#includ "main.h"
+/**
+ * print_rev - prints string in revers
+ * @s: pointer to string
+ * Return - void
+ */
+
+void print_rev(char *s)
+{
+int i = 0;
+while (s[i])
+{
+i++
+}
+int a = i;
+for (int j = a; j == 0; j--)
+{
+_putchar(s[a]);
+}
+_putchar('\n');
+}
