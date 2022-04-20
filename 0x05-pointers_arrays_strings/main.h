497444f7185819c8205d2a80680324ae6985e1e4
abbot00 <abbot_jude@outlook.com>
2022-04-20 13:48:30 +0000

    task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
  *this file contains prototypes of all funcitons
  *used in this folder
  */
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
