f410d4f28395ee82792454792f394c9d78f7fe76
abbot00 <abbot_jude@outlook.com>
2022-04-21 10:02:08 +0000

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts_half - writes the second half of a string
+ *  @str: pointer to string
+ * Return: void.
+ */
+
+
+void puts_half(char *str)
+{
+int n, len, i;
+len = 0;
+while (str[i] != '\0')
+{
+len++;
+}
+if (len % 2 == 0)
+{
+for (i = len / 2; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+}
+else
+{
+for (n = (len - 1) / 2; n < len - 1; n++)
+{
+_putchar(str[n + 1]);
+}
+}
+_putchar('\n');
+}
