a88461b063124f12e6fddc7ebda822e385b99597
abbot00 <abbot_jude@outlook.com>
2022-04-21 13:49:16 +0000

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,9 +8,20 @@ int _atoi(char *s)
 {
 int result = 0;
 int i;
+int sig = -1;
+int brk = 0;
 for (i = 0; s[i] != '\0'; ++i)
 {
+if (s[i] == '-')
+sig = sig * -1;
+if (s[i] >= '0' && s[i] <= '9')
+{
 result = result * 10 + s[i] - '0';
+brk = 1;
 }
-return result;
+else if (brk == 1)
+break;
+}
+result = sig * result;
+return (result);
 }
