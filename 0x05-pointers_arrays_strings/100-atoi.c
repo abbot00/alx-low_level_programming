e6b220de18a00e88906b304f7e97043ccd09aaea
abbot00 <abbot_jude@outlook.com>
2022-04-21 13:36:12 +0000

    task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+
+/**
+ * _atoi - converts strings to integers
+ * @s: Pointer to string
+ * Return - the integer output
+ */
+int _atoi(char *s)
+{
+int result = 0;
+for (int i = 0; s[i] != '\0'; ++i)
+{
+result = result * 10 + s[i] - '0';
+}
+return result;
+}
