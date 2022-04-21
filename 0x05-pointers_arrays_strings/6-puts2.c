ccf9097d17466a77d1492522f0ef62ec377402b1
abbot00 <abbot_jude@outlook.com>
2022-04-21 09:41:26 +0000

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a string. 
+ * puts2 - prints every other character of a string.
  * @str: pointer to string.
  * Return - Void
  */
