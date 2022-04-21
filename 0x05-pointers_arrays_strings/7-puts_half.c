5cacfbb712b09103158f3eae69bd63ef36550358
abbot00 <abbot_jude@outlook.com>
2022-04-21 10:10:35 +0000

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,14 +10,13 @@ void puts_half(char *str)
 {
 int n, len, i;
 len = 0;
-i = 0;
-n = 0;
 while (str[i] != '\0')
 {
 len++;
 }
 if (len % 2 == 0)
 {
+i = 0;
 for (i = len / 2; str[i] != '\0'; i++)
 {
 _putchar(str[i]);
