fc2e9652b34e52300049027978565dee9d442387
abbot00 <abbot_jude@outlook.com>
2022-04-26 11:49:41 +0000

    taks 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+/**
+ * _strpbrk - searches for a set of bytes
+ * @s: pointer to the string
+ * @accept: pointer to string
+ * Return: pointer to the byt in s
+ * that matches accept.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (accept[i] == *s)
+{
+return (s);
+}
+}
+s++;
+}
+return (NULL);
+}
