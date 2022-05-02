aafb6fee2c04f2af872ee69790d291b7602ce5e7
abbot00 <abbot_jude@outlook.com>
2022-05-02 19:33:04 +0000

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - prints the name of program
- * @argc - argc
- * @argv - argv
+ * @argc: argc
+ * @argv: argv
  * Return: zero if ran correctly
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 if (argc > 0)
 printf("%s\n", argv[0]);
