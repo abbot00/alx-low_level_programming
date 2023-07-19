6f2da2015876144d8553202dc6f59a66c9867b58
JUDE UCHECHUKWU OBIEKEZIE <abbot_jude@outlook.com>
2023-07-19 13:35:57 +0100

    Update 3-islower.c
    changes made with a better understanding of loops in c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,18 +5,17 @@
  * @c: contains value to be compared
  * Return: Always 0.
  */
+
 int _islower(int c)
-
 {
-
-if (c > 'a' && c < 'z')
+int ret;
+if (c >= 'a' && c <= 'z')
 {
-return (1);
+ret = 1;
 }
-
 else
 {
-return (0);
+ret = 0;
 }
-
+return (ret);
 }
