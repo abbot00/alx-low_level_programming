75c63391c96d231f8082983421cd9229d02661a7
JUDE UCHECHUKWU OBIEKEZIE <abbot_jude@outlook.com>
2023-07-22 10:00:37 +0100

    Update 100-times_table.c a better output

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,54 @@
 * print_times_table - prints times table
 *Return: void
 */
+
 void print_times_table(int n)
+{
+int i, rev_prod, multi, prod;
+if (n > 15 || n < 0)
+{
+return;
+}
+i = 0;
+while (i <= n)
+{
+multi = 0;
+while (multi <= n)
+{
+prod = i * multi;
+if (prod == 0)
+{
+_putchar('0');
+}
+else
+{
+rev_prod = 0;
+while( prod != 0)
+{
+rev_prod = rev_prod * 10 + prod % 10;
+prod = prod / 10;
+}
+while (rev_prod != 0)
+{
+_putchar(rev_prod % 10 + '0');
+rev_prod /= 10;
+}
+}
+if (multi != n)
+{
+_putchar(',');
+_putchar(' ');
+}
+multi++;
+}
+putchar('\n');
+i++;
+}
+}
+
+
+
+/*void print_times_table(int n)
 {
 int a = 0, rep, b;
 if (n < 0 || n > 15)
@@ -44,3 +91,16 @@ _putchar('\n');
 a++;
 }
 }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
