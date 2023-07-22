b50c42794ef170aeebc294963ae5a5a2a577499c
JUDE UCHECHUKWU OBIEKEZIE <abbot_jude@outlook.com>
2023-07-22 10:16:50 +0100

    Update 100-times_table.c code trial

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,8 +5,45 @@
 *n - number of times to be produced.
 *Return: void
 */
+void print_times_table(int n) {
+    if (n > 15 || n < 0) {
+        return;
+    }
+    int i = 0;
+    while (i <= n) {
+        putchar('0');
+        putchar(',');
+        putchar(' ');
+        int j = 1;
+        while (j <= n) {
+            int num = i * j;
+            if (num < 10) {
+                putchar(' ');
+                putchar(' ');
+                putchar(num + '0');
+            } else if (num < 100) {
+                putchar(' ');
+                putchar(num / 10 + '0');
+                putchar(num % 10 + '0');
+            } else {
+                putchar(num / 100 + '0');
+                putchar((num / 10) % 10 + '0');
+                putchar(num % 10 + '0');
+            }
+            if (j != n) {
+                putchar(',');
+                putchar(' ');
+            }
+            j++;
+        }
+        putchar('\n');
+        i++;
+    }
+}
 
-void print_times_table(int n)
+
+
+/*void print_times_table(int n)
 {
 int i, rev_prod, multi, prod;
 if (n > 15 || n < 0)
@@ -48,4 +85,5 @@ multi++;
 _putchar('\n');
 i++;
 }
-}
+} 
+*/
