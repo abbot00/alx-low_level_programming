aabf6436f7277ff60104c839e24a87813fac9a50
JUDE UCHECHUKWU OBIEKEZIE <abbot_jude@outlook.com>
2023-07-23 21:32:15 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,19 @@
+#include "main.h"
 /**
  * _strlen - finds the length of a string
  * @s: Pointer to the string
  * Return: returns an int, the lenght of string
  */
 
+
+
 int _strlen(char *s)
 {
-int a = 0;
-while (s[a])
-a++;
-return (a);
-
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return length;
 }
