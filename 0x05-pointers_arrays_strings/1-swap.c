4385338fb8864f12549fefcb1cb65ac7cf0a5756
JUDE UCHECHUKWU OBIEKEZIE <abbot_jude@outlook.com>
2023-07-23 21:22:11 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,15 @@
+#include "main.h"
 /**
  * swap_int -  swaps value of a and b;
  * @a: - pointer
  * @b: - pointer
  * Return - void value
  */
+
 void swap_int(int *a, int *b)
 {
-*a += *b;
-*b = *a - *b;
-*a = *a - *b;
+int temp = *a;
+*a = *b;
+*b = temp;
+return;
 }
