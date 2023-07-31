f6262ec80073779c19928f9408b9f4726350597f
JUDE UCHECHUKWU OBIEKEZIE <abbot_jude@outlook.com>
2023-07-31 12:44:53 +0100

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,47 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * print_opcodes - Prints the opcodes of the main function.
- * num_bytes: Number of bytes to print from the main function.
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 1 or 2 on error
  */
-void print_opcodes(int num_bytes) {
-    if (num_bytes < 0) {
+int main(int argc, char *argv[])
+{
+    int i, num_bytes;
+    char *ptr;
+
+    if (argc != 2)
+    {
+        printf("Error\n");
+        exit(1);
+    }
+
+    num_bytes = atoi(argv[1]);
+    if (num_bytes < 0)
+    {
         printf("Error\n");
         exit(2);
     }
 
-    /* Obtain the address of the main function as a pointer to unsigned char. */
-    unsigned char *main_ptr = (unsigned char *)print_opcodes;
-    
-    /* Loop through the specified number of bytes and print the opcodes. */
-    int i;
-    for (i = 0; i < num_bytes; i++) {
-        printf("%02x ", main_ptr[i]);
+    ptr = (char *)main;
+    for (i = 0; i < num_bytes; i++)
+    {
+        printf("%02x", (unsigned char)ptr[i]);
+        if (i != num_bytes - 1)
+            printf(" ");
     }
     printf("\n");
-}
 
-/*
- * main - Entry point of the program.
- * argc: Number of command-line arguments.
- * argv: Array of pointers to the command-line arguments.
- *
- * Return: 0 if successful, 1 if incorrect number of arguments, and 2 if negative number of bytes.
- */
-int main(int argc, char *argv[]) {
-    int num_bytes;
-
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
-
-    /* Convert the argument to an integer. */
-    num_bytes = atoi(argv[1]);
-
-    /* Print the opcodes of the main function. */
-    print_opcodes(num_bytes);
-
-    return 0;
+    return (0);
 }
