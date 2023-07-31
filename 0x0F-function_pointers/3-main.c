744f4ee0222e5ee0bbf24da9d21e7e6aa02aa88f
JUDE UCHECHUKWU OBIEKEZIE <abbot_jude@outlook.com>
2023-07-31 12:20:31 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,36 +10,36 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    char *operator;
-    int (*operation)(int, int);
+int num1, num2, result;
+char *operator;
+int (*operation)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return 98;
-    }
+if (argc != 4)
+{
+printf("Error\n");
+return 98;
+}
 
-    num1 = atoi(argv[1]);
-    operator = argv[2];
-    num2 = atoi(argv[3]);
+num1 = atoi(argv[1]);
+operator = argv[2];
+num2 = atoi(argv[3]);
 
-    operation = get_op_func(operator);
+operation = get_op_func(operator);
 
-    if (operation == NULL)
-    {
-        printf("Error\n");
-        return 99;
-    }
+if (operation == NULL)
+{
+printf("Error\n");
+return 99;
+}
 
-    if ((*operator == '/' || *operator == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        return 100;
-    }
+if ((*operator == '/' || *operator == '%') && num2 == 0)
+{
+printf("Error\n");
+return 100;
+}
 
-    result = operation(num1, num2);
-    printf("%d\n", result);
+result = operation(num1, num2);
+printf("%d\n", result);
 
-    return 0;
+return 0;
 }
