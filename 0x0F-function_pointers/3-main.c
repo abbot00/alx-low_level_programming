3e2410b762ab1f928cc07b6d2a72df2fd0b72735
JUDE UCHECHUKWU OBIEKEZIE <abbot_jude@outlook.com>
2023-07-31 12:22:24 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,7 +17,7 @@ int (*operation)(int, int);
 if (argc != 4)
 {
 printf("Error\n");
-return 98;
+return (98);
 }
 
 num1 = atoi(argv[1]);
@@ -29,17 +29,17 @@ operation = get_op_func(operator);
 if (operation == NULL)
 {
 printf("Error\n");
-return 99;
+return (99);
 }
 
 if ((*operator == '/' || *operator == '%') && num2 == 0)
 {
 printf("Error\n");
-return 100;
+return (100);
 }
 
 result = operation(num1, num2);
 printf("%d\n", result);
 
-return 0;
+return (0);
 }
