38c97acb0a661e1b10cff5b2f47b532b3eb1f53a
JUDE UCHECHUKWU OBIEKEZIE <abbot_jude@outlook.com>
2023-07-31 12:50:27 +0100

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,30 +10,28 @@
  */
 int main(int argc, char *argv[])
 {
-    int i, num_bytes;
-    char *ptr;
+int i, num_bytes;
+char *ptr;
+if (argc != 2)
+printf("Error\n");
+exit(1);
+}
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
+num_bytes = atoi(argv[1]);
+if (num_bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
 
-    num_bytes = atoi(argv[1]);
-    if (num_bytes < 0)
-    {
-        printf("Error\n");
-        exit(2);
-    }
+ptr = (char *)main;
+for (i = 0; i < num_bytes; i++)
+{
+printf("%02x", (unsigned char)ptr[i]);
+if (i != num_bytes - 1)
+printf(" ");
+}
+printf("\n");
 
-    ptr = (char *)main;
-    for (i = 0; i < num_bytes; i++)
-    {
-        printf("%02x", (unsigned char)ptr[i]);
-        if (i != num_bytes - 1)
-            printf(" ");
-    }
-    printf("\n");
-
-    return (0);
+return (0);
 }
