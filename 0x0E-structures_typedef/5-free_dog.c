7d2ffc137babc3077adacf7f33149592dafb52d9
JUDE UCHECHUKWU OBIEKEZIE <abbot_jude@outlook.com>
2023-07-31 07:26:45 +0100

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,16 +3,16 @@
 
 /**
  * free_dog - frees a dog structure
- * @d: pointer to struct to free
- * Return: Nothing
+ * @d: pointer to dog structure to free
+ *
+ * Return: void
  */
 void free_dog(dog_t *d)
 {
-  if (d == NULL)
-    return;
-  if (d->name != NULL)
-    free(d->name);
-  if (d->owner != NULL)
-    free(d->owner);
-  free(d);
+if (d == NULL)
+return;
+
+free(d->name);
+free(d->owner);
+free(d);
 }
